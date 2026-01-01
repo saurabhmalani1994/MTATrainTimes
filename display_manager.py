e3f233cdc9ccd8586cc8014f0007305b2e2be475
Saurabh  Malani <saurabh_malani@fermeate.onmicrosoft.com>
2026-01-01 17:29:15 -0500

    Change fonts again 4

--- display_manager.py
+++ display_manager.py
@@ -97,9 +97,9 @@ class DisplayManager:
             # "/usr/share/fonts/truetype/nunito-sans/NunitoSans-VariableFont_YTLC,opsz,wdth,wght.ttf",
             # "/usr/share/fonts/truetype/noto/NotoSansMono-Regular.ttf",
             # "/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf",
-            "/usr/share/fonts/truetype/freefont/FreeSans.ttf",
+            # "/usr/share/fonts/truetype/freefont/FreeSans.ttf",
             # "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
-            # "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf",
+            "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf",
         ]
         
         # First try to find OpenSans specifically
