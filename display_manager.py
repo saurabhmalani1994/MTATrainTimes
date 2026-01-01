f6dab768bf53452f0880250824b29e3bad618766
Saurabh  Malani <saurabh_malani@fermeate.onmicrosoft.com>
2026-01-01 17:28:55 -0500

    Change fonts again 3

--- display_manager.py
+++ display_manager.py
@@ -96,8 +96,8 @@ class DisplayManager:
         font_paths = [
             # "/usr/share/fonts/truetype/nunito-sans/NunitoSans-VariableFont_YTLC,opsz,wdth,wght.ttf",
             # "/usr/share/fonts/truetype/noto/NotoSansMono-Regular.ttf",
-            "/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf",
-            # "/usr/share/fonts/truetype/freefont/FreeSans.ttf",
+            # "/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf",
+            "/usr/share/fonts/truetype/freefont/FreeSans.ttf",
             # "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
             # "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf",
         ]
